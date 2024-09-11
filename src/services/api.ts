74a7b7490af718de5b0bfcc577e9a416b6cf5707
psyks17 <shroffkrish140@gmail.com>
2024-09-11 22:30:27 +0530

    api change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://paletteaiserver.onrender.com/api';
 
 export const extractColors = async (url: string) => {
   const response = await axios.post(`${API_BASE_URL}/extract-colors`, { url });
